f30cb36801cf1ecb67e967c9b75f3981d11ee784
reinaldogez <reinaldogez@inf.ufsm.br>
2023-03-30 18:30:17 -0300

    created user and userLog domain entities, added gitignore file

---
+++ src/CqrsShowCase/Domain/User.cs
@@ -0,0 +1,8 @@
+namespace CqrsShowCase.Domain;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<UserLog> Logs { get; set; }
+}
---
+++ src/CqrsShowCase/Domain/UserLog.cs
@@ -0,0 +1,10 @@
+namespace CqrsShowCase.Domain;
+
+public class UserLog
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Action { get; set; }
+    public DateTime Timestamp { get; set; }
+    public User User { get; set; }
+}
