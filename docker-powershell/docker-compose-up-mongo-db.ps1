148d5d2be236d47d432f5937fdf9e28ac21d988a
Reinaldo Gez <reinaldogez@inf.ufsm.br>
2024-03-20 21:41:50 -0300

    added docker-compose for mongo db

---
+++ docker-powershell/docker-compose-up-mongo-db.ps1
@@ -0,0 +1,3 @@
+# run this command from cqrs-showcase root folder
+# .\docker-powershell\docker-compose-up-mongo-db.ps1
+docker-compose -f .\docker-compose-mongo-db.yaml up -d
--- docker-powershell/docker-compose-up-ms-sql.ps1
+++ docker-powershell/docker-compose-up-ms-sql.ps1
@@ -1,3 +1,3 @@
-# .\docker-powershell\docker-compose-up-ms-sql.ps1
 # run this command from cqrs-showcase root folder
+# .\docker-powershell\docker-compose-up-ms-sql.ps1
 docker-compose -f .\docker-compose-ms-sql.yaml up -d
